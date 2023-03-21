8908bb470aa626aedb6e78c76ab60bfb11096015
Saulo Wilson de Araujo Silva <saulo.silva@sulamerica.com.br>
2023-03-21 15:20:40 -0300

    Remoção do Scroll da página sobre

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -9,7 +9,7 @@ export default function Sobre() {
                     <h1 className="w-56 font-montserrat font-bold text-sm lg:text-base text-start text-cinza-escuro uppercase tracking-widest">Sobre mim</h1>
                     <hr className="bg-cinza h-[2px] w-full" />
                 </div>
-                <div className="flex flex-col gap-4 md:h-screen md:overflow-scroll font-montserrat font-normal text-sm lg:text-base text-cinza-medio">
+                <div className="flex flex-col gap-4 md:h-screen font-montserrat font-normal text-sm lg:text-base text-cinza-medio">
                     <p>Sou uma desenvolvedora Front End com conhecimentos sólidos em HTML5, CSS3 e Javascript. Além dessas habilidades, também possuo conhecimentos em React, TailwindCSS e Bootstrap. Como uma entusiasta da tecnologia e amante de design, minha paixão pela programação me levou a me especializar em desenvolvimento Front End, onde tenho a oportunidade de unir meu amor pela criatividade e habilidades técnicas para criar experiências web incríveis.</p>
                     <p>Estou constantemente me atualizando em relação às melhores práticas e tendências do mercado para oferecer soluções inovadoras para meus clientes. Adoro trabalhar em equipe e estou sempre em busca de novos desafios para me aprimorar como profissional e pessoa. </p>
                     <p>Se você precisa de uma desenvolvedora Front End altamente qualificada e apaixonada pelo que faz, não hesite em entrar em contato comigo. Estou animada para ajudar a transformar suas ideias em realidade!</p>
